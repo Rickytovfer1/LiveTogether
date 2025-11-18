ea9a7212d58cdb9799c97336a1b13b8922ee641c
Ricardo <tovar.fernandez.ricardo@iescamas.es>
2025-11-18 13:09:28 +0100

    Creacion del proyecto backend

---
+++ Backend-LiveTogether/src/main/java/org/example/backendlivetogether/BackendLiveTogetherApplication.java
@@ -0,0 +1,13 @@
+package org.example.backendlivetogether;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendLiveTogetherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendLiveTogetherApplication.class, args);
+	}
+
+}
---
+++ Backend-LiveTogether/src/test/java/org/example/backendlivetogether/BackendLiveTogetherApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.backendlivetogether;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendLiveTogetherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
