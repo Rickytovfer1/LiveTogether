4e6e53bef827b2a857d38e85a0a0af937b9056d8
Ricardo <tovar.fernandez.ricardo@iescamas.es>
2026-05-21 11:08:20 +0200

    Merge realizado con la rama develop

--- frontend-live-together/src/app/app.routes.ts
+++ frontend-live-together/src/app/app.routes.ts
@@ -2,9 +2,13 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
+    path: 'home',
+    loadComponent: () => import('./home/home.page').then((m) => m.HomePage),
+  },
+  {
     path: '',
-    redirectTo: 'inicio-sesion',
-    pathMatch: 'full'
+    redirectTo: 'home',
+    pathMatch: 'full',
   },
 
   {
